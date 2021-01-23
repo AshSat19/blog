679f5eacabc2bd1e9838f6a80fbfa8fea43d2472
Ashwin Sathian <ashwinsathyan19@gmail.com>
2021-01-24 00:57:08 +0530

    .

--- src/app/client/components/client-header/client-header.component.ts
+++ src/app/client/components/client-header/client-header.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ClientHeaderComponent implements OnInit {
   @Input() title: string = 'Ashwin Sathian\'s Blog'
-  @Input() mastHeadCover: string = 'https://images.unsplash.com/photo-1471107340929-a87cd0f5b5f3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1266&q=80'
+  @Input() mastHeadCover: string | undefined = 'https://images.unsplash.com/photo-1471107340929-a87cd0f5b5f3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1266&q=80'
 
   constructor() {}
 
