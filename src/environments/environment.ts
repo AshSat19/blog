aff5899749955cb6734a4e17bc3376ba20c29d49
Ashwin Sathian <ashwinsathyan19@gmail.com>
2021-01-24 12:33:28 +0530

    Updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,6 +13,9 @@ export const environment = {
     appId: '1:1044272290847:web:7dacc04d88bd8ba98e092a',
   },
   apiURL: 'http://localhost:3000/api/',
+  blogName: 'Ashwin Sathian\'s Blog',
+  authorName: 'Ashwin Sathian',
+  mastHeadCoverImage: 'https://images.unsplash.com/photo-1471107340929-a87cd0f5b5f3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1266&q=80'
 };
 
 /*
