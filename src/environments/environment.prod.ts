788175f53abe1aadb4e454d101f7f7fdd2345ada
Ashwin Sathian <ashwinsathyan19@gmail.com>
2021-01-24 12:42:43 +0530

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,4 +9,7 @@ export const environment = {
     appId: '1:1044272290847:web:7dacc04d88bd8ba98e092a'
   },
   apiURL: 'https://peaceful-headland-96005.herokuapp.com/api/',
+  blogName: 'Ashwin Sathian\'s Blog',
+  authorName: 'Ashwin Sathian',
+  mastHeadCoverImage: 'https://images.unsplash.com/photo-1471107340929-a87cd0f5b5f3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1266&q=80'
 };
